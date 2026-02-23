dc4048508252b268a7daeb4247537ec807268738
SunnyDev <cristinavranceanu86@gmail.com>
2026-02-23 12:15:43 +0100

    update project

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -11,7 +11,7 @@ const ServiceCard = ({ icon, title, description, features, className }) => {
       <div className="text-center">
         <i className={`${icon} fs-1 text-success `}></i>
         <h5 className=" ">{title}</h5>
-        <p className="card-text">{description}</p>
+        {/* <p className="card-text">{description}</p> */}
         {/* <ul className="list-unstyled text-start mt-3">
           {features.map((feature, idx) => (
             <li key={idx} className="mb-1">• {feature}</li>
